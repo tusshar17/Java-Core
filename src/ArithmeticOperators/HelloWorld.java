be737f9bb9edbc8b625f0b7dd3913cacab5a9269
tusshar17 <tr.jakhara019@gmail.com>
2023-10-25 13:09:14 +0530

    Arithmetic Operators

---
+++ src/ArithmeticOperators/HelloWorld.java
@@ -0,0 +1,31 @@
+package ArithmeticOperators;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 2;
+
+        // addition
+        int addition = a+b;
+        System.out.println("Addition: " + addition);
+
+        // subtraction
+        int subtraction = a-b;
+        System.out.println("Subtraction: " + subtraction);
+
+        // multiplication
+        int multiplication = a*b;
+        System.out.println("Multiplication: " + multiplication);
+
+        // division
+        int division = a/b;
+        System.out.println("Division: " + division);
+
+        // modulus
+        int modulus = a%b;
+        System.out.println("Modulus: " + modulus);
+
+    }
+}
