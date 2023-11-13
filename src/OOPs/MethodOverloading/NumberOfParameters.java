a8f9455c6abbcaf8de8bcc0ea7a1501e8b4649f3
tusshar17 <tr.jakhara019@gmail.com>
2023-11-13 11:19:34 +0530

    Method overloading in terms of number of parameter

--- src/OOPs/MethodOverloading/NumberOfParameters.java
+++ src/OOPs/MethodOverloading/NumberOfParameters.java
@@ -1,6 +1,24 @@
 package OOPs.MethodOverloading;
 
+
+class OverloadingNumberOfParams{
+
+    void check(int a, int b){
+        System.out.println("Number of params: 2");
+    }
+
+    void check(int a, int b, int c){
+        System.out.println("Number of params: 3");
+    }
+}
+
 public class NumberOfParameters {
 
+    public static void main(String[] args) {
 
+        OverloadingNumberOfParams obj = new OverloadingNumberOfParams();
+
+        obj.check(1, 2);
+        obj.check(1, 2, 3);
+    }
 }
