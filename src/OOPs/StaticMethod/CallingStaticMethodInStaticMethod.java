f3cc183e26fd5282ed4e649ad6f1ca6dbda4d0a5
tusshar17 <tr.jakhara019@gmail.com>
2023-11-15 15:00:41 +0530

    calling static method

--- src/OOPs/StaticMethod/CallingStaticMethodInStaticMethod.java
+++ src/OOPs/StaticMethod/CallingStaticMethodInStaticMethod.java
@@ -1,8 +1,25 @@
 package OOPs.StaticMethod;
 
+
+class Demo{
+    static void SayHi(){
+        System.out.println("Ho, from a static method of external class.");
+    }
+}
+
 public class CallingStaticMethodInStaticMethod {
 
+
+    // static method inside same class
+    static void sayHello(){
+        System.out.println("Hello, from a static method inside same class.");
+    }
+
     public static void main(String[] args) {
-        
+        // calling static method of same class
+        sayHello();
+
+        // calling static method of external class
+        Demo.SayHi();
     }
 }
