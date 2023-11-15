d69751fabccff22a7ed05a05be3a91366bd665af
tusshar17 <tr.jakhara019@gmail.com>
2023-11-15 16:46:16 +0530

    calling static method inside instance method

--- src/OOPs/StaticMethod/CallingStaticMethodInInstanceMethod.java
+++ src/OOPs/StaticMethod/CallingStaticMethodInInstanceMethod.java
@@ -1,2 +1,39 @@
-package OOPs.StaticMethod;public class CallingStaticMethodInInstanceMethod {
+package OOPs.StaticMethod;
+
+class Example{
+    static void sayHi(){
+        System.out.println("Hi, from a static method of external class.");
+    }
+
+
+}
+
+
+public class CallingStaticMethodInInstanceMethod {
+
+    // static method inside same class
+    static void sayHello(){
+        System.out.println("Hello, from a static method inside same class.");
+    }
+
+
+    void instanceMethod(){
+
+        // calling internal static method
+        sayHello();
+
+        // calling external static method using class name
+        Example.sayHi();
+
+        // calling external static method using object name
+        Example obj = new Example();
+        obj.sayHi();
+
+    }
+
+
+    public static void main(String[] args) {
+        CallingStaticMethodInInstanceMethod obj2 = new CallingStaticMethodInInstanceMethod();
+        obj2.instanceMethod();
+    }
 }
