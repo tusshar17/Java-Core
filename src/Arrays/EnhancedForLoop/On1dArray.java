64bdf1b48d8e929e7cadedc6a2cd4fce511109d9
tusshar17 <tr.jakhara019@gmail.com>
2023-11-19 13:01:25 +0530

    EnhancedForLoop

---
+++ src/Arrays/EnhancedForLoop/On1dArray.java
@@ -0,0 +1,17 @@
+package Arrays.EnhancedForLoop;
+
+public class On1dArray {
+
+
+    public static void main(String[] args) {
+
+        int[] num = {1,2,3,4,5};
+
+        for (int i :
+                num) {
+            System.out.println(i);
+        }
+
+
+    }
+}
