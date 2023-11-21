bc60b68d79829a67044c0c571593c0c04c78e478
tusshar17 <tr.jakhara019@gmail.com>
2023-11-21 16:28:15 +0530

    Default Constructor

--- src/OOPs/Constructor/DefaultConstructor.java
+++ src/OOPs/Constructor/DefaultConstructor.java
@@ -1,2 +1,19 @@
-package OOPs.Constructor;public class DefaultConstructor {
+package OOPs.Constructor;
+
+
+class DemoClass{
+
+    // default constructor
+    DemoClass(){
+        System.out.println("default constructor called");
+    }
+}
+
+
+ 
+public class DefaultConstructor {
+
+    public static void main(String[] args) {
+        DemoClass obj = new DemoClass();
+    }
 }
