92c02ef63e548b785246aa8383b41872347fb81a
tusshar17 <tr.jakhara019@gmail.com>
2024-01-19 04:15:09 +0530

    final class

---
+++ src/OOPs/FinalKeyword/Final_Class.java
@@ -0,0 +1,19 @@
+package OOPs.FinalKeyword;
+
+
+final class Father{
+    void sayHello(){
+        System.out.println("Hello world");
+    }
+}
+
+/*
+--- we can not inherit a final class ---
+class Son extends Father{
+
+}
+
+ */
+
+public class Final_Class {
+}
